3c5887c9edec17c9df9b898d3a6970d563832339
nebiyu youhannes <nebiyuyouhannes08@gmail.com>
2024-02-19 09:56:44 +0300

    added the import nn module

--- going_modular/prediction.py
+++ going_modular/prediction.py
@@ -1,9 +1,10 @@
+import torch
+import torchvision
 from torchvision import transforms
 from PIL import Image
 from typing import Tuple,List
-import torch
-import torchvision
 import matplotlib.pyplot as plt
+import torch.nn as nn
 
 def custom_predictions(
     model:nn.Module,
@@ -13,33 +14,39 @@ def custom_predictions(
     device:torch.device="cpu",
     image_transform:torchvision.transforms=None,
   ):
+    
+    
     """ this function is used for displaying the image and the label predicted by the model"""
-  #open image
-  img=Image.open(img_dir)
+    #open image
 
-  if image_transform is not None:
-    transorm=image_transform
-  else:
-    #make the transform manually
-    transform=transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Resize(img_size),
-        transforms.Normalize(
-            mean=[0.485, 0.456, 0.406],
-            std=[0.229, 0.224, 0.225]
-        )
-    ])
+    img=Image.open(img_dir)
 
 
-  model.to(device)
-  model.eval()
-  with torch.inference_mode():
-    transformed_image=transform(img)
-    y_pred_logits=model(transformed_image.unsqueeze(dim=0))
-    y_pred=torch.argmax(torch.softmax(y_pred_logits,dim=1),dim=1)
-    label_pred=class_names[y_pred]
+    if image_transform is not None:
+      transorm=image_transform
 
-  plt.figure()
-  plt.imshow(img)
-  plt.title(f"pred {label_pred} | prob {y_pred_logits.max():.2f}")
-  plt.axis("OFF")
+    else:
+
+      #make the transform manually
+      transform=transforms.Compose([
+          transforms.ToTensor(),
+          transforms.Resize(img_size),
+          transforms.Normalize(
+              mean=[0.485, 0.456, 0.406],
+              std=[0.229, 0.224, 0.225]
+          )
+      ])
+    
+
+    model.to(device)
+    model.eval()
+    with torch.inference_mode():
+      transformed_image=transform(img)
+      y_pred_logits=model(transformed_image.unsqueeze(dim=0))
+      y_pred=torch.argmax(torch.softmax(y_pred_logits,dim=1),dim=1)
+      label_pred=class_names[y_pred]
+
+    plt.figure()
+    plt.imshow(img)
+    plt.title(f"pred {label_pred} | prob {y_pred_logits.max():.2f}")
+    plt.axis("OFF")
